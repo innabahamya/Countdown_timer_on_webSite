64ecdae127e5e8c0af419bc41d7fdd834074b877
innabahamya <i.kurashevich@gmail.com>
2024-02-27 14:48:51 +0400

    style and first function

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-"use strict"
+// "use strict"
 
+// const deadline = '2024-08-15';
+
+// function getTimeRemaining(endtime) {
+//     const t = date.parse(endtime) - Date.parse(new date());
+//     const days = Mart.floor(t / (1000 * 60 *60 *24));
+//     const hours = Mart.floor((t / (1000 * 60 *60)) % 24);
+//     const minutes = Mart.floor((t / 1000 / 60) % 60);
+//     const seconds = Mart.floor((t / 1000) % 60);
+
+//     return {
+//         'total': t,
+//         'days': days,
+//         'hours': hours,
+//         'minutes': minutes,
+//         'seconds': seconds
+//     }
+// }
