5ca10511d2384685c9235fffef471579f3524a1e
innabahamya <i.kurashevich@gmail.com>
2024-02-28 18:11:50 +0400

    add if for past date

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 "use strict"
 
-const deadline = '2024-08-15';
+const deadline = '2024-05-15';
 
 function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
     const t = Date.parse(endtime) - Date.parse(new Date());
-    const days = Math.floor(t / (1000 * 60 *60 *24));
-    const hours = Math.floor((t / (1000 * 60 *60)) % 24);
-    const minutes = Math.floor((t / 1000 / 60) % 60);
-    const seconds = Math.floor((t / 1000) % 60);
+
+    if (t <= 0) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    } else {
+        days = Math.floor(t / (1000 * 60 *60 *24));
+        hours = Math.floor((t / (1000 * 60 *60)) % 24);
+        minutes = Math.floor((t / 1000 / 60) % 60);
+        seconds = Math.floor((t / 1000) % 60);
+    }  
 
     return {
         'total': t,
@@ -18,7 +27,15 @@ function getTimeRemaining(endtime) {
     };
 }
 
-    function setClock(selector, endtime) {
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) {
         const timer = document.querySelector(selector);
         const days = document.querySelector('#days');
         const hours = document.querySelector('#hours');
@@ -30,18 +47,18 @@ function getTimeRemaining(endtime) {
         // вызов функции без моргания
         updateClock(); 
 
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
+    function updateClock() {
+         const t = getTimeRemaining(endtime);
 
-            days.textContent = t.days;
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+        days.textContent = getZero(t.days);
+        hours.textContent = getZero(t.hours);
+        minutes.textContent = getZero(t.minutes);
+        seconds.textContent = getZero(t.seconds);
 
-            if (t.total<= 0) {
-                clearInterval(timeInterval);
-            }
+        if (t.total<= 0) {
+            clearInterval(timeInterval);
         }
     }
+}
 
-    setClock('.timer', deadline);
+setClock('.timer', deadline);
