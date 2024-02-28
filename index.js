1ceb0df503a4463f43f0e2861a7b9000c52db391
innabahamya <i.kurashevich@gmail.com>
2024-02-28 17:35:14 +0400

    function setClock

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-// "use strict"
+"use strict"
 
-// const deadline = '2024-08-15';
+const deadline = '2024-08-15';
 
-// function getTimeRemaining(endtime) {
-//     const t = date.parse(endtime) - Date.parse(new date());
-//     const days = Mart.floor(t / (1000 * 60 *60 *24));
-//     const hours = Mart.floor((t / (1000 * 60 *60)) % 24);
-//     const minutes = Mart.floor((t / 1000 / 60) % 60);
-//     const seconds = Mart.floor((t / 1000) % 60);
+function getTimeRemaining(endtime) {
+    const t = date.parse(endtime) - Date.parse(new date());
+    const days = Mart.floor(t / (1000 * 60 *60 *24));
+    const hours = Mart.floor((t / (1000 * 60 *60)) % 24);
+    const minutes = Mart.floor((t / 1000 / 60) % 60);
+    const seconds = Mart.floor((t / 1000) % 60);
 
-//     return {
-//         'total': t,
-//         'days': days,
-//         'hours': hours,
-//         'minutes': minutes,
-//         'seconds': seconds
-//     }
-// }
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = document.querySelector('#days');
+        const hours = document.querySelector('#hours');
+        const minutes = document.querySelector('#minutes');
+        const seconds = document.querySelector('#seconds');
+    }
+}
